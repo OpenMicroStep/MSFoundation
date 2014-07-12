e27f7fd16109a64e7c3da98898f97aa25450aebb
Eric Baradat <e-baradat@logitud.fr>
2014-07-12 15:19:34 +0200

    MSSystemInitialize dans main de MHServer et MHNetRepositoryServer

--- MHServer_src/MASHServer_main.m
+++ MHServer_src/MASHServer_main.m
@@ -1,30 +1,30 @@
 /*
-
+ 
  MASHServer_main.m
-
+ 
  This file is is a part of the MicroStep Application Server over Http Application.
-
+ 
  Initial copyright LOGITUD Solutions (logitud@logitud.fr) since 2012
-
+ 
  Herve Malaingre : herve@malaingre.com
  Jean-Michel Bertheas :  jean-michel.bertheas@club-internet.fr
-
-
+ 
+ 
  This software is a computer program whose purpose is to [describe
  functionalities and technical features of your software].
-
+ 
  This software is governed by the CeCILL-C license under French law and
  abiding by the rules of distribution of free software.  You can  use,
  modify and/ or redistribute the software under the terms of the CeCILL-C
  license as circulated by CEA, CNRS and INRIA at the following URL
  "http://www.cecill.info".
-
+ 
  As a counterpart to the access to the source code and  rights to copy,
  modify and redistribute granted by the license, users are provided only
  with a limited warranty  and the software's author,  the holder of the
  economic rights,  and the successive licensors  have only  limited
  liability.
-
+ 
  In this respect, the user's attention is drawn to the risks associated
  with loading,  using,  modifying and/or developing or reproducing the
  software by the user in light of its specific status of free software,
@@ -35,14 +35,14 @@
  requirements in conditions enabling the security of their systems and/or
  data to be ensured and,  more generally, to use and operate it in the
  same conditions as regards security.
-
+ 
  The fact that you are presently reading this means that you have had
  knowledge of the CeCILL-C license and that you accept its terms.
-
+ 
  A Special homage to Steve Jobs who permits the Objective-C technology
  to meet the world. Without him, this years-long work should not have
  been existing at all. Thank you Steve and rest in peace.
-
+ 
  */
 
 #import <MSNet/MSNet.h>
@@ -54,32 +54,34 @@
 
 MSExport void ** OPENSSL_Applink(void)
 {
-    return _OPENSSL_Applink() ;
+  return _OPENSSL_Applink() ;
 }
 #endif
 
-NSArray *parametersFromArgs(int argc, const char *argv[])
+static NSArray *parametersFromArgs(int argc, const char *argv[])
 {
-    NSMutableArray *params = [NSMutableArray array] ;
-    int i = 0 ;
-    
-    for(i=0; i<argc; i++)
-    {
+  NSMutableArray *params = [NSMutableArray array] ;
+  int i = 0 ;
+  
+  for(i=0; i<argc; i++)
+  {
 #ifdef WO451
-        [params addObject:[[NSString alloc] initWithCString:argv[i] length:strlen(argv[i])]] ;
+    [params addObject:[[NSString alloc] initWithCString:argv[i] length:strlen(argv[i])]] ;
 #else
-
-        [params addObject:[[NSString alloc] initWithBytes:argv[i] length:strlen(argv[i]) encoding:NSUTF8StringEncoding]] ;
+    
+    [params addObject:[[NSString alloc] initWithBytes:argv[i] length:strlen(argv[i]) encoding:NSUTF8StringEncoding]] ;
 #endif
-    }
-    return params ;
+  }
+  return params ;
 }
 
 int main(int argc, const char * argv[])
 {
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init] ;
-    
-    MSUInt result = MHStartBundleServer(parametersFromArgs(argc, argv)) ;
-    [pool release] ;
-    return result;
+  MSUInt result;
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init] ;
+  
+  MSSystemInitialize(0, NULL);
+  result = MHStartBundleServer(parametersFromArgs(argc, argv)) ;
+  [pool release] ;
+  return (int)result;
 }
