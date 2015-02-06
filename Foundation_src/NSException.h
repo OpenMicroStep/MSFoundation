ad23277deb85ba415fb7733cfc779a9d2bf6f90d
Vincent Rouillé <v-rouille@logitud.fr>
2015-02-06 17:37:19 +0100

    NSException first steps

--- Foundation_src/NSException.h
+++ Foundation_src/NSException.h
@@ -12,6 +12,31 @@ FoundationExtern NSString * const NSObjectInaccessibleException;
 FoundationExtern NSString * const NSObjectNotAvailableException;
 FoundationExtern NSString * const NSDestinationInvalidException;
 
-@interface NSException : NSObject
+@interface NSException : NSObject <NSCopying, NSCoding> {
+    @private
+    NSString		*_name;
+    NSString		*_reason;
+    NSDictionary	*_userInfo;
+    id			reserved;
+}
+
++ (NSException *)exceptionWithName:(NSString *)name reason:(NSString *)reason userInfo:(NSDictionary *)userInfo;
+- (instancetype)initWithName:(NSString *)aName reason:(NSString *)aReason userInfo:(NSDictionary *)aUserInfo;
+
+- (NSString *)name;
+- (NSString *)reason;
+- (NSDictionary *)userInfo;
+
+- (NSArray *)callStackReturnAddresses;
+- (NSArray *)callStackSymbols;
+
+- (void)raise;
+
+@end
+
+@interface NSException (NSExceptionRaisingConveniences)
+
++ (void)raise:(NSString *)name format:(NSString *)format, ...;
++ (void)raise:(NSString *)name format:(NSString *)format arguments:(va_list)argList;
 
 @end
--- Foundation_src/NSException.m
+++ Foundation_src/NSException.m
@@ -13,4 +13,47 @@ NSString * const NSDestinationInvalidException= @"NSDestinationInvalidException"
 
 @implementation NSException
 
++ (NSException *)exceptionWithName:(NSString *)name reason:(NSString *)reason userInfo:(NSDictionary *)userInfo
+{
+  return AUTORELEASE([ALLOC(self) initWithName:name reason:reason userInfo:userInfo]);
+}
+
+- (instancetype)initWithName:(NSString *)name reason:(NSString *)reason userInfo:(NSDictionary *)userInfo
+{
+  _name= [name retain];
+  _reason= [reason retain];
+  _userInfo= [userInfo retain];
+  return self;
+}
+
+- (NSString *)name         { return _name;}
+- (NSString *)reason       { return _reason; }
+- (NSDictionary *)userInfo { return _userInfo; }
+
+- (NSArray *)callStackReturnAddresses
+{
+  return [self notYetImplemented:_cmd];
+}
+- (NSArray *)callStackSymbols
+{
+  return [self notYetImplemented:_cmd];
+}
+
+- (void)raise
+{
+  @throw self;
+}
+
++ (void)raise:(NSString *)name format:(NSString *)format, ...
+{
+  va_list va;
+  va_start(va, format);
+  [self raise:name format:format arguments:va];
+}
+
++ (void)raise:(NSString *)name format:(NSString *)format arguments:(va_list)argList
+{
+  [[self exceptionWithName:name reason:AUTORELEASE([ALLOC(NSString) initWithFormat:format arguments:argList]) userInfo:nil] raise];
+}
+
 @end
