4038131f4288e1c1530f4722200c35e2e7fcc3b8
Vincent Rouillé <v-rouille@logitud.fr>
2015-02-18 18:11:31 +0100

    MSDictionary foundation compatibility enhancements

--- Foundation_src/NSDictionary.m
+++ Foundation_src/NSDictionary.m
@@ -1,5 +1,13 @@
 #import "FoundationCompatibility_Private.h"
 
+@interface _MSSDictionary : MSDictionary
+// Immutable version of MSDictionary with some changes to follow NSDictionary specs
+@end
+
+@interface _MSMDictionary : MSDictionary
+// Mutable version of MSDictionary with some changes to follow NSMutableDictionary specs
+@end
+
 @implementation NSDictionary
 + (void)load {MSFinishLoadingAddClass(self);}
 + (void)finishLoading
@@ -35,28 +43,6 @@
 { [self notImplemented:_cmd]; return 0; }
 @end
 
-@interface _MSMDictionary : MSDictionary
-@end
-
-@implementation _MSMDictionary
-
-- (Class)superclass
-{
-    return [NSMutableDictionary class];
-}
-
-- (BOOL)isKindOfClass:(Class)aClass
-{
-    return (aClass == [NSMutableDictionary class]) || [super isKindOfClass:aClass];
-}
-- (instancetype)initWithCapacity:(NSUInteger)capacity{
-  if((self= [self init])) {
-    CDictionaryGrow((CDictionary*)self, capacity);
-  }
-  return self;
-}
-@end
-
 @implementation NSMutableDictionary
 + (instancetype)allocWithZone:(NSZone *)zone
 {
@@ -76,4 +62,34 @@
 { [self notImplemented:_cmd]; }
 - (void)setObject:(id)anObject forKey:(id <NSCopying>)aKey
 { [self notImplemented:_cmd]; }
+@end
+
+@implementation _MSSDictionary
+-(id)copyWithZone:(NSZone *)zone
+{ return [self retain]; }
+-(id)mutableCopyWithZone:(NSZone *)zone
+{ return [ALLOC(NSMutableDictionary) initWithDictionary:self]; }
+@end
+
+@implementation _MSMDictionary
+-(id)copyWithZone:(NSZone *)zone
+{ return [ALLOC(NSDictionary) initWithDictionary:self]; }
+-(id)mutableCopyWithZone:(NSZone *)zone
+{ return [ALLOC(NSMutableDictionary) initWithDictionary:self]; }
+
+- (Class)superclass
+{ 
+  return [NSMutableDictionary class]; 
+}
+- (BOOL)isKindOfClass:(Class)aClass
+{
+  return (aClass == [NSMutableDictionary class]) || [super isKindOfClass:aClass];
+}
+
+- (instancetype)initWithCapacity:(NSUInteger)capacity{
+  if((self= [self init])) {
+    CDictionaryGrow((CDictionary*)self, capacity);
+  }
+  return self;
+}
 @end
