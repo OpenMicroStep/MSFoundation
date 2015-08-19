e460add064988420bfe63029b4b59fef9f0b833b
Vincent Rouillé <v-rouille@logitud.fr>
2015-08-19 10:55:04 +0200

    Enhance NSRunLoop (provide main runloop that matches uv_default_loop())

--- Foundation_src/NSRunLoop.m
+++ Foundation_src/NSRunLoop.m
@@ -3,35 +3,46 @@
 NSString* const NSDefaultRunLoopMode= @"default";
 NSString* const NSRunLoopCommonModes= @"common";
 
-@implementation NSRunLoop
-
 static void __currentRunLoop_dtor(void *runloop) {
   [(NSRunLoop*)runloop release];
 }
 MS_DECLARE_THREAD_LOCAL(__currentRunLoop, __currentRunLoop_dtor);
 
+@implementation NSRunLoop
+
++ (void)initialize
+{
+  if (self == [NSRunLoop class]) {
+    NSRunLoop *mainRunLoop= [ALLOC(NSRunLoop) initWithLoop:uv_default_loop()];
+    tss_set(__currentRunLoop, mainRunLoop);}
+}
+
 + (NSRunLoop *)currentRunLoop
 {
   NSRunLoop *loop= tss_get(__currentRunLoop);
   if(!loop) {
-    tss_set(__currentRunLoop, [NSRunLoop new]);
-  }
+    tss_set(__currentRunLoop, [ALLOC(NSRunLoop) initWithLoop:NULL]);}
   return loop;
 }
-- (instancetype)init {
-  _uv_loop= MSMallocFatal(sizeof(uv_loop_t), "NSRunLoop uv_loop_t");
-  uv_loop_init(_uv_loop);
+- (instancetype)initWithLoop:(uv_loop_t *)loop {
+  if(!loop) {
+    loop= (uv_loop_t *)MSMallocFatal(sizeof(uv_loop_t), "NSRunLoop uv_loop_t");
+    uv_loop_init(loop);}
+  _uv_loop= loop;
   return self;
 }
 - (void)dealloc {
-  uv_loop_close(_uv_loop);
-  MSFree(_uv_loop, "NSLoop uv_loop_t");
+  if (_uv_loop != uv_default_loop()) {
+    uv_loop_close(_uv_loop);
+    MSFree(_uv_loop, "NSLoop uv_loop_t");}
   [super dealloc];
 }
+
 - (void)addTimer:(NSTimer *)aTimer forMode:(NSString *)mode
 {
   [aTimer _addToLoop:_uv_loop];
 }
+
 - (void)run
 {
   while(uv_run(_uv_loop, UV_RUN_DEFAULT))
@@ -47,7 +58,6 @@ MS_DECLARE_THREAD_LOCAL(__currentRunLoop, __currentRunLoop_dtor);
 {
   return uv_stop(_uv_loop);
 }
-
 
 - (uv_loop_t *)_uv_loop
 {
