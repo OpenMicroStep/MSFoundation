7291e7e361ccd50e53dd9bbd7f647452825dabbe
Vincent Rouillé <v-rouille@logitud.fr>
2015-11-09 10:58:00 +0100

    [Fix] [MSCore] Fix CCouple test warnings

--- MSCore_tst/mscore_ccouple_validate.c
+++ MSCore_tst/mscore_ccouple_validate.c
@@ -19,22 +19,22 @@ static void ccouple_create(test_t *test)
   TASSERT_ISEQUAL(test, CCoupleFirstMember(d), c, "A5-MSC1(d) & c not equals");
   TASSERT_ISEQUAL(test, d, CCoupleSecondMember(e), "A6-d & CCoupleSecondMember(e) not equals");
 
-  CCoupleSetFirstMember(e, d);
+  CCoupleSetFirstMember(e, (id)d);
   TASSERT(test, !CCoupleEquals(f, e), "f & e are not equals anymore");
-  TASSERT_EQUALS_PTR(test, CCoupleFirstMember(e), d);
-  TASSERT_EQUALS_PTR(test, CCoupleSecondMember(e), d);
+  TASSERT_EQUALS_PTR(test, CCoupleFirstMember(e), (id)d);
+  TASSERT_EQUALS_PTR(test, CCoupleSecondMember(e), (id)d);
   TASSERT_EQUALS_LLD(test, RETAINCOUNT(c), 3);
   TASSERT_EQUALS_LLD(test, RETAINCOUNT(d), 4);
 
-  CCoupleSetSecondMember(e, c);
+  CCoupleSetSecondMember(e, (id)c);
   TASSERT(test, !CCoupleEquals(f, e), "f & e are not equals anymore");
-  TASSERT_EQUALS_PTR(test, CCoupleFirstMember(e), d);
-  TASSERT_EQUALS_PTR(test, CCoupleSecondMember(e), c);
+  TASSERT_EQUALS_PTR(test, CCoupleFirstMember(e), (id)d);
+  TASSERT_EQUALS_PTR(test, CCoupleSecondMember(e), (id)c);
   TASSERT_EQUALS_LLD(test, RETAINCOUNT(c), 4);
   TASSERT_EQUALS_LLD(test, RETAINCOUNT(d), 3);
 
-  CCoupleSetFirstMember(f, d);
-  CCoupleSetSecondMember(f, c);
+  CCoupleSetFirstMember(f, (id)d);
+  CCoupleSetSecondMember(f, (id)c);
   TASSERT(test, CCoupleEquals(f, e), "f & e are equals again");
 
   RELEASE(c);RELEASE(d);RELEASE(e);RELEASE(f);
@@ -46,18 +46,18 @@ static void ccouple_toarray(test_t *test)
   c0= CCreateCouple(  nil,  nil);
   c1= CCreateCouple((id)c0,  nil);
   c2= CCreateCouple((id)c0,(id)c1);
-  a0= CCreateArrayOfCoupleSubs(c0, nil);
-  a1= CCreateArrayOfCoupleSubs(c1, nil);
-  a2= CCreateArrayOfCoupleSubs(c2, nil);
+  a0= CCreateArrayOfCoupleSubs((id)c0, nil);
+  a1= CCreateArrayOfCoupleSubs((id)c1, nil);
+  a2= CCreateArrayOfCoupleSubs((id)c2, nil);
 
   TASSERT_EQUALS_LLD(test, CArrayCount(a0), 0);
 
   TASSERT_EQUALS_LLD(test, CArrayCount(a1), 1);
-  TASSERT_EQUALS_PTR(test, CArrayObjectAtIndex(a1, 0), c0);
+  TASSERT_EQUALS_PTR(test, CArrayObjectAtIndex(a1, 0), (id)c0);
 
   TASSERT_EQUALS_LLD(test, CArrayCount(a2), 2);
-  TASSERT_EQUALS_PTR(test, CArrayObjectAtIndex(a2, 0), c0);
-  TASSERT_EQUALS_PTR(test, CArrayObjectAtIndex(a2, 1), c1);
+  TASSERT_EQUALS_PTR(test, CArrayObjectAtIndex(a2, 0), (id)c0);
+  TASSERT_EQUALS_PTR(test, CArrayObjectAtIndex(a2, 1), (id)c1);
 
   RELEASE(c0);RELEASE(c1);RELEASE(c2);
   RELEASE(a0);RELEASE(a1);RELEASE(a2);
